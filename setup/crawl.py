5130c1d6b054f32ae38f0e007f72b1df1ae4060c
Suibhne Ó Foighil <suibhneof1@gmail.com>
2023-09-27 21:05:44 -0400

    crawler v1 with good filter

--- setup/crawl.py
+++ setup/crawl.py
@@ -3,6 +3,7 @@ import openai
 import os
 import json
 import datetime
+import csv
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
@@ -17,7 +18,6 @@ def get_search_queries(name):
     f"{name} townhall",
     f"{name} speech",
     f"{name} press conference",
-    f"{name} lecture"
   ]
   
   return queries
@@ -36,7 +36,7 @@ POLITICIANS = [
 ]
 
 class YouTubeCrawler:
-    def __init__(self):
+    def __init__(self, log=False):
         self.api_key = None
         self.youtube = None
         self.politician = None
@@ -45,6 +45,8 @@ class YouTubeCrawler:
         self.num_raw_results = 0
         self.num_results = None
         self.results = None
+        self.log = log
+        self.ids = {}
     
     def set_creds(self):
         self.api_key = os.environ['YOUTUBE_API_KEY']
@@ -62,73 +64,132 @@ class YouTubeCrawler:
     def set_num_results(self, num_results):
         self.num_results = num_results
 
+    def parse_gpt_choice(self, choice: dict) -> dict:
+        resp = {}
+        text = choice['text']
+        #find start of json
+        start = text.find("{")
+        #find end of json
+        end = text.rfind("}")
+        #get json
+        json_str = text[start:end+1]
+        #strip newlines and spaces
+        json_str = json_str.replace("\n", "")
+        x = json_str.strip()
+
+        #parse json
+        try:
+            resp = json.loads(x)
+        except json.decoder.JSONDecodeError as e:
+            print("Error parsing JSON:")
+            print(e)
+            print("JSON string:")
+            print(x)
+        return resp
+
     def apply_content_filter(self, result, k=3) -> bool:
         # setup prompt to only accept content where the politican is speaking and not taken out of context
         politician = self.politician
+        #TODO: implement voting system
         content_filter_prompt = f"""
-        You are a political analyst, interested in understanding the views of the politician, {politician}. You will be given a video to evaluate. Your goal is to determine whether the content is valuable or not. You only want to see content where the politician is speaking and not taken out of context. The following content is permissible:
+        You are a research analyst, interested in gathering footage of {politician} speaking. You will be given a video to evaluate. Your goal is to determine whether the content is good enough for your research. You only want to see content where the politician is the primary speaker.
+        
+        % Examples of Permissible Content %
         - Speeches by {politician}
         - Interviews with {politician}
         - Remarks by {politician}
         - Townhalls with {politician}
         - Press conferences with {politician}
-        - other Videos of {politician} expressing their views
-        The following content is not permissible:
-        - Hyperpartisan content
+        - other Videos of {politician} speaking
+        % Examples of Impermissible Content %
+        - Hyperpartisan / Hypercritical content
+        - Videos of other people talking about {politician}
         - Videos that take {politician}'s words out of context
         - Videos that are not about {politician}
-        - Clickbait videos
+        - Videos where {politician} is not the primary speaker
 
-        Data will be presented in the following format:
-        <Video Title>
-        <Video Description>
-        <Video Channel Title>
-        <Video Published At>
+        First, describe the reasons why the content is permissible or not. Then, answer the following question: Is this video permissible? (y/n). Always return your evaluation as a single JSON object, enclosed in curly brakets. For example:
+        
+        % Example %
+        Title: {politician} speaks at [event]
+        Description: {politician} addresses the crowd at [event]
+        Channel Title: {politician} News Network
+        Published At: 2021-01-01
+        {{"reasoning": "This video is a speech by {politician}. It is permissible.", "permissible": "y"}}
 
-        % Data %
-        <Video Title> {result['title']} </Video Title>
-        <Video Description> {result['description']} </Video Description>
-        <Video Channel Title> {result['channelTitle']} </Video Channel Title>
-        <Video Published At> {result['publishedAt']} </Video Published At>
-        % End Data %
-
-        Please describe the content of the video. Then, answer the following question: Is this video permissible? (y/n). Return your answer as a JSON object: {{"description": "The video is a speech by {politician} about the economy", "permissible": "y"}}
+        % Your Response %
+        Title: {result['title']}
+        Description: {result['description']}
+        Channel Title: {result['channelTitle']}
+        Published At: {result['publishedAt']}
         """
 
-        # # create openAI endpoint
-        results = openai.Completion.create(
+        # create openAI endpoint
+        result = openai.Completion.create(
             model="gpt-3.5-turbo-instruct",
             prompt=content_filter_prompt,
-            n=3,
+            max_tokens=1000,
+            n=k
         )
-        choices = results['choices']
-        responses = [ choice['text'] for choice in choices ]
 
-        # get majority vote
-        yes = responses.count('y')
-        no = responses.count('n')
-        return yes > no
+        choices = result['choices']
+        json_responses = [ self.parse_gpt_choice(choice) for choice in choices ]
+
+        votes = [ json.get('permissible', None) for json in json_responses ]
+        reasons = [ json.get('reasoning', None) for json in json_responses ]
+
+        num_yes = votes.count('y')
+        num_no = votes.count('n')
+        is_permissible = num_yes > num_no
+
+        if self.log:
+            print("Content Filter Call:")
+            print("JSON Responses:")
+            print(json_responses)
+            print("Votes:")
+            print(votes)
+            print("Reasons:")
+            print(reasons)
+            print("Is Permissible:")
+            print(is_permissible)
+
+        return is_permissible
 
     def run(self):
-        # request = self.youtube.search().list(
-        #     part='snippet',
-        #     q=self.query,
-        #     type='video',
-        #     maxResults=self.num_results,
-        #     order='relevance',
-        #     videoDuration=self.length
-        # )
-        # response = request.execute()
+        # try:
+        #     request = self.youtube.search().list(
+        #         part='snippet',
+        #         q=self.query,
+        #         type='video',
+        #         maxResults=self.num_results,
+        #         order='relevance',
+        #         videoDuration=self.length,
+        #         videoCaption='closedCaption'
+        #     )
+        #     response = request.execute()
+        # except Exception as e:
+        #     print("Error executing request:")
+        #     print(e)
+        #     print("Query:")
+        #     print(self.query)
+        #     print("Length:")
+        #     print(self.length)
+        #     return
+        # TODO: handle HTTPError 403 better
 
         # For testing
         cwrd = os.getcwd()
-        path = os.path.join(cwrd, 'setup/ex.json')
+        path = os.path.join(cwrd, 'setup/tests/joe-biden-press-conference-50-long/data.json')
         with open(path, 'r') as f:
             response = json.load(f)
         # End testing
 
         items = response['items']
         snippets = [ item['snippet'] for item in items ]
+
+        #TODO: check if video is already in database
+        videoIds = [ item['id']['videoId'] for item in items ]
+        
         raw_results = [ {
             'title': snippet['title'],
             'description': snippet['description'],
@@ -142,7 +203,7 @@ class YouTubeCrawler:
 
         # Filter out bad content
         evals = [ self.apply_content_filter(result) for result in raw_results ]
-        results = [ result for result, val in zip(raw_results, evals) if not val ]
+        results = [ result for result, val in zip(raw_results, evals) if val ]
 
         self.results = results
     
@@ -150,17 +211,18 @@ class YouTubeCrawler:
         return self.results
 
 class Crawler:
-    def __init__(self, db):
+    def __init__(self, db, log=False):
         self.db = db
-        self.crawler = YouTubeCrawler()
+        self.crawler = YouTubeCrawler(log=log)
         self.crawler.set_creds()
+        self.log = log
 
-    def run(self, politician, query, k=50, length='medium', log=False):
+    def run(self, politician, query, k=50, length='medium'):
+        if self.log:
+            print("Running crawl...")
+            print(f"Query: {query}")
+            print(f"Video length: {length} ({k} results)")
         
-        if log:
-            print(f"Running query: {query}")
-            initial_len = len(self.db)
-
         self.crawler.set_politician(politician)
         self.crawler.set_query(query)
         self.crawler.set_length(length)
@@ -170,38 +232,48 @@ class Crawler:
         for result in results:
             self.db.add(result)
 
-        if log:
+        if self.log:
             print("Results:")
             print(f"Video length: {length} ({k} results)")
             print(f"Number of raw results: {self.crawler.num_raw_results}")
-            print(f"Number of results: {len(results)}")
-            print(f"Number of new results: {len(self.db) - initial_len}")
+            print(f"Number of filtered results: {len(results)}")
 
             #save to log file
-            with open('crawl_log.txt', 'a') as f:
+            path = os.path.join(os.getcwd(), 'setup/crawl_log.txt')
+            with open(path, 'a') as f:
                 f.write(f"Query: {query}\n")
                 f.write(f"Video length: {length} ({k} results)\n")
                 f.write(f"Number of raw results: {self.crawler.num_raw_results}\n")
-                f.write(f"Number of results: {len(results)}\n")
-                f.write(f"Number of new results: {len(self.db) - initial_len}\n")
+                f.write(f"Number of filtered results: {len(results)}\n")
                 f.write("\n")
 
-    def save(self, log=False):
-        if log:
+    def save(self):
+        if self.log:
             print("Saving crawl...")
 
         #Get collection
-        collection = self.db.store.get(include=['documents'])
+        collection = self.db.store.get(include=['metadatas'])
 
-        #Save as csv
+        #Save to csv
         current_date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        path = os.path.join(os.getcwd(), f'crawls/{current_date}.csv')
-        with open('crawl.csv', 'w') as f:
-            f.write("title,description,channelTitle,publishedAt,videoId\n")
-            for document in collection['documents']:
-                f.write(f"{document['title']},{document['description']},{document['channelTitle']},{document['publishedAt']},{document['videoId']}\n")
+        path = os.path.join(os.getcwd(), f'setup/crawls/{current_date}.csv')
+        with open(path, 'w+') as f:
+
+            writer = csv.writer(f)
+
+            metadatas = collection['metadatas']
+            ids = collection['ids']
+
+            #Write header
+            headers = ['videoId'] + list(metadatas[0].keys())
+            writer.writerow(headers)
+
+            #Write each row
+            for videoId, metadata in zip(ids, metadatas):
+                row = [videoId] + list(metadata.values())
+                writer.writerow(row)
         
-        if log:
+        if self.log:
             print("Done saving crawl.")
 
 
@@ -219,52 +291,43 @@ class Database:
     def is_same(self, result1, result2, cosine_similarity, k=3, log=False) -> bool:
         #TODO: this doesn't work at all?
         similarity_filter_prompt = f"""
-        You will be given two videos to evaluate. Your goal is to determine whether they are about the same event or not. Please describe the similarities and differences between the two videos. Then, answer the following question: Are these videos about the same event? (y/n)
+        You will be given two videos to evaluate. Your goal is to determine whether they cover the same exact event. For instance, both videos could be about the same speech, interview, or press conference.
 
-        Data will be presented in the following format:
-        <Video 1 Title> | <Video 2 Title>
-        <Video 1 Description> | <Video 2 Description>
-        <Video 1 Channel Title> | <Video 2 Channel Title>
-        <Video 1 Published At> | <Video 2 Published At>
-        <Cosine Similarity>
-
-        % Data %
-        Video 1 Title: {result1['title']} | Video 2 Title: {result2['title']}
-        Video 1 Description: {result1['description']} | Video 2 Description: {result2['description']}
-        Video 1 Channel Title: {result1['channelTitle']} | Video 2 Channel Title: {result2['channelTitle']}
-        Video 1 Published At: {result1['publishedAt']} | Video 2 Published At: {result2['publishedAt']}
+        <Video 1>
+        Title: {result1['title']}
+        Description: {result1['description']}
+        Channel Title: {result1['channelTitle']}
+        Published At: {result1['publishedAt']}
+        </Video 1>
+        <Video 2>
+        Title: {result2['title']}
+        Description: {result2['description']}
+        Channel Title: {result2['channelTitle']}
+        Published At: {result2['publishedAt']}
+        </Video 2>
         Cosine Similarity: {cosine_similarity}
-        % End Data %
 
-        Describe the similarities and differences between the two videos. Then, answer the following question: Are these videos about the same event? (y/n). Return your answer as a JSON object: {{"description": "The titles are roughly the same in both videos and both were published on December 1st by different media outlets", "same": "y"}}
+        Describe the similarities and differences between the two videos. Then, answer the following question: Are these videos cover the same event? (y/n). Return your answer as a JSON object: {{"description": "The videos cover the same interview", "same": "y"}}
         """
 
         # create openAI endpoint
         results = openai.Completion.create(
             model="gpt-3.5-turbo-instruct",
             prompt=similarity_filter_prompt,
-            n=k,
+            max_tokens=500
         )
 
-        responses = [ result['text'] for result in results['choices'] ]
-        print("Responses:")
-        print(responses)
-        print(type(responses))
-        
-        descriptions = [ response['description'] for response in responses ]
-        votes = [ response['same'] for response in responses ]
+        response = results['choices'][0]['text']
+        print("Similarity Filter Call:")
+        print("Response:")
+        print(response)
+        json_response = self.parse_gpt_choice(results)
+        print("JSON Response:")
+        print(json_response)
 
-        if log:
-            print("Responses:")
-            for response in responses:
-                print(response)
-            print("Votes:")
-            print(votes)
+        is_same = json_response['same'] == 'y'
+        return is_same
 
-        # get majority vote
-        yes = votes.count('y')
-        no = votes.count('n')
-        return yes > no
 
     def get_embedding(self, text, model="text-embedding-ada-002"):
         text = text.replace("\n", " ")
@@ -280,35 +343,37 @@ class Database:
         """
         xq = self.get_embedding(q)
 
-        if len(self) > 0:
-            #Check 5 most similar results
-            k = 5
-            most_similar_results = self.store.query(
-                query_embeddings=[xq],
-                include=['metadatas', 'distances'],
-                n_results=k
-            )
+        # #CHECK FOR DUPLICATES
+        # if len(self) > 0:
+        #     #Check 5 most similar results
+        #     k = 5
+        #     most_similar_results = self.store.query(
+        #         query_embeddings=[xq],
+        #         include=['metadatas', 'distances'],
+        #         n_results=k
+        #     )
 
-            #Evaluate similarity
-            metadatas = most_similar_results['metadatas']
-            distances = most_similar_results['distances']
-            print("Most similar results:")
-            print(metadatas)
-            print(distances)
-            for metadata, distance in zip(metadatas, distances):
+        #     #Evaluate similarity
+        #     metadatas = most_similar_results['metadatas']
+        #     distances = most_similar_results['distances']
+        #     print("Most similar results:")
+        #     print(metadatas)
+        #     print(distances)
+        #     for metadata, distance in zip(metadatas, distances):
 
-                md = metadata[0]
-                dist = distance[0]
-                print(md)
-                print(dist)
+        #         md = metadata[0]
+        #         dist = distance[0]
+        #         print(md)
+        #         print(dist)
 
-                cosine_similarity = 1 - dist
+        #         cosine_similarity = 1 - dist
 
-                is_same = self.is_same(result, md, cosine_similarity)
-                if is_same:
-                    #If same, don't add
-                    return
+        #         is_same = self.is_same(result, md, cosine_similarity)
+        #         if is_same:
+        #             #If same, don't add
+        #             return
 
+        #TODO: handle HTTPError 403 better
         #Remove videoId
         videoId = result.pop('videoId')
     
@@ -322,7 +387,9 @@ class Database:
 # Path: setup/crawl.py
 if __name__ == '__main__':
     db = Database()
-    crawl = Crawler(db)
+    crawl = Crawler(db, log=True)
+
+    #TODO: implement tdqm progress bar
     # for politician in POLITICIANS:
     #     queries = get_search_queries(politician)
     #     for query in queries:
@@ -331,18 +398,24 @@ if __name__ == '__main__':
     #                 politician=politician,
     #                 query=query, 
     #                 k=50, 
-    #                 length=length, 
-    #                 log=True
+    #                 length=length
     #             )
 
     #For testing
     crawl.run(
-        politician="Donald Trump",
-        query="Donald Trump interview", 
+        politician="Joe Biden",
+        query="Joe Biden speech", 
         k=50, 
-        length='medium', 
-        log=True
+        length='long',
+        save=True
+    )
+    crawl.run(
+        politician="Joe Biden",
+        query="Joe Biden speech", 
+        k=50, 
+        length='long',
+        save=True
     )
     #End testing
 
-    crawl.save(log=True)
+    crawl.save()
